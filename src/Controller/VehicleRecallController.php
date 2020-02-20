0ceb881de881e2d115c7f66db2014a4171e6b4a8
Lawrence Owens <laowensjr@gmail.com>
2020-02-20 05:12:27 -0600

    WORKING

--- src/Controller/VehicleRecallController.php
+++ src/Controller/VehicleRecallController.php
@@ -3,19 +3,49 @@
 
 namespace App\Controller;
 
-
-use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Response;
+
 
 class VehicleRecallController
 {
 
     /**
-     * @Route("/nhtsa-recall/vehicle/details")
+     * @Route("/nhtsa-recall")
      */
     public function recallVehicle()
-        {
-            return new Response('Recalled Vehicle Details');
+    {
+        return new Response('Welcome to the Recall Genie');
 
-        }
+    }
+
+    /**
+     * @Route("/nhtsa-recall/vehicle/details/")
+     */
+     public function show()
+        {
+            $postdata = null;
+            $opts = array('http' =>
+                array(
+                    'method' => 'GET',
+                    'content' => $postdata
+                )
+            );
+
+            $apiURL = "https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/440?format=json";
+            $context = stream_context_create($opts);
+            $fp = fopen($apiURL, 'rb', false, $context);
+            if(!$fp)
+            {
+                echo "in first if";
+            }
+            $myresponse = @stream_get_contents($fp);
+            if($myresponse == false)
+            {
+                echo "in second if";
+            }
+           // echo $myresponse;
+return new JsonResponse(json_decode($myresponse));
+    }
 }
